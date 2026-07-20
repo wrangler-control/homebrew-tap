67530b36e7bec2c0cdd6207e3d3e0c7b9337e115
RmnMatusevich <47831494+RmnMatusevich@users.noreply.github.com>
2026-07-20 16:46:17 +0200

    Mark GitHub download URL as verified (brew audit)

--- Casks/wrangler.rb
+++ Casks/wrangler.rb
@@ -4,7 +4,8 @@ cask "wrangler" do
   sha256 arm:   "8d0a7ae1feb29132399f869c0ee2dcf42f9669b35771462a6dfa3fc4f93aec74",
          intel: "65509969bb67fd4d589b7cd5cdaf4597dbcf0696a3a1610d64b3e7a9fd6db6b2"
 
-  url "https://github.com/wrangler-control/releases/releases/download/v#{version}/Wrangler_#{version}_#{arch}.dmg"
+  url "https://github.com/wrangler-control/releases/releases/download/v#{version}/Wrangler_#{version}_#{arch}.dmg",
+      verified: "github.com/wrangler-control/releases/"
   name "Wrangler"
   desc "Control tower for engineers running parallel coding agents"
   homepage "https://wrangler-control.com/"
